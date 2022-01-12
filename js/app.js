f14518c336895043d5e541b5c2c7e0e529055be4
ali mohsen <ali.mhsen@gmail.com>
2022-01-12 21:54:37 +0300

    first commit1

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ function reg() {
     var password = document.getElementById("password").value;
     var re_password = document.getElementById("re_password").value;
     var age = document.getElementById("age").value;
+    var phone = document.getElementById("phone").value;
     var url = document.getElementById("url").value;
     var massage = document.getElementById("massage").value;
 
@@ -23,11 +24,41 @@ function reg() {
         document.getElementById("name_masseg").innerHTML = "";
         
     }
+    /* && /[A-Z]+/.test(password) && /[1-9]+/.test(password) && /\W+/.test(password) */
+    /* ^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$ 
+    [a-z]+[A-Z]+[1-9]+\W+*/
     
-    if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))) {
+    if (!(/\S+@\S+\.\S+$/.test(email))) {
         document.getElementById("name_masseg").innerHTML = "email sentacs is nout true";
         return false;
     }else
     document.getElementById("name_masseg").innerHTML = "";
+
+    
+    
+    if (!(/[a-z]+/.test(password) && /[A-Z]+/.test(password) && /[1-9]+/.test(password) && /\W+/.test(password))) {
+        document.getElementById("name_masseg").innerHTML = "The password most contain capital and smale liters, number, and character";
+        return false;
+    }else
+    document.getElementById("name_masseg").innerHTML = "";
+
+    if (!(password==re_password)) {
+        document.getElementById("name_masseg").innerHTML = "re-pasword is not similer password";
+        return false;
+    }else
+    document.getElementById("name_masseg").innerHTML = "";
+
+    if (!(age > 18)) {
+        document.getElementById("name_masseg").innerHTML = "The age most be grater than 18 years old";
+        return false;
+    }else
+    document.getElementById("name_masseg").innerHTML = "";
+
+    if (!(/^777\d{6}$/.test(phone))) {
+        document.getElementById("name_masseg").innerHTML = "The number most beging by 777 and most mor than 9 numbers";
+        return false;
+    }else
+    document.getElementById("name_masseg").innerHTML = "";
+
 
 }
